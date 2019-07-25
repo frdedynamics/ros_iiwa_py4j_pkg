fa77dc3ed432f258407b723698ba5904a2e4ae08
Johannes Møgster <johannes.mogster@mojotech.no>
2019-07-25 15:48:33 +0200

    first

---
+++ iiwa_py4j_pkg/scripts/measured_joint_state.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+""" module docstring, yo! """
+
+import sys
+import rospy
+from py4j.java_gateway import JavaGateway
+
+from sensor_msgs.msg import JointState
+
+GATEWAY = JavaGateway()
+
+FRI = GATEWAY.entry_point
+
+
+def talker():
+    """ docsctring, yo! """
+
+    # print "getMediaFlangeInputX3Pin10: " + str(FRI.getMediaFlangeInputX3Pin10())
+
+    # print "getMeasuredJointPosition: " + str([round(x,3) for x in list(FRI.getMeasuredJointPosition())])
+
+    # print "getCommandedJointPosition: " + str([round(x,3) for x in list(FRI.getCommandedJointPosition())])
+
+    # print "getIpoJointPosition: " + str([round(x,3) for x in list(FRI.getIpoJointPosition())])
+
+    # print "getMeasuredTorque: " + str([round(x,3) for x in list(FRI.getMeasuredTorque())])
+
+    # print "getCommandedTorque: " + str([round(x,3) for x in list(FRI.getCommandedTorque())])
+
+    # print "getExternalTorque: " + str([round(x,3) for x in list(FRI.getExternalTorque())])
+    
+    joint_state_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
+    rospy.init_node('kuka_joint_state', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
+    while not rospy.is_shutdown():
+        try:
+            msg = JointState()
+            tmp = FRI.getTimeStampSecNanoSec()
+            msg.header.stamp.secs = tmp[0]
+            msg.header.stamp.nsecs = tmp[1]
+            msg.name = ['joint_a1','joint_a2','joint_a3','joint_a4','joint_a5','joint_a6','joint_a7']
+            msg.position = list(FRI.getMeasuredJointPosition())
+            # msg.velocity = list(FRI.getMeasuredJointPosition())
+            msg.effort = list(FRI.getMeasuredTorque())
+
+            joint_state_pub.publish(msg)
+        except Exception as exception_e:
+            rospy.logerr("%s", exception_e)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    MYARGV = rospy.myargv(argv=sys.argv)
+
+    print("args I don't care about: ", MYARGV)
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
